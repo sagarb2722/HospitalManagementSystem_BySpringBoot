317e83a8f062688f5dfc583925ad0e57581f8138
sagarb2722 <sagarb2722@gmail.com>
2022-12-02 12:33:50 +0530

    hospitral projec

---
+++ src/main/java/com/ty/hospital/hospitalmanagement_springboot/HospitalmanagementSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.ty.hospital.hospitalmanagement_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalmanagementSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalmanagementSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ty/hospital/hospitalmanagement_springboot/HospitalmanagementSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ty.hospital.hospitalmanagement_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalmanagementSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
